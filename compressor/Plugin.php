4abed87644e8f2944c5f70b1d93d0a77ba1258ba
Vallabh Kansagara <vrkansagara@gmail.com>
2020-07-31 07:47:06 +0530

    adding plugines

---
+++ compressor/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace i3web\compressor;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ compressor/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'i3web-compressor',
+        'description' => ''
+    ]
+];
